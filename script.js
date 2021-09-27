b7f1e360766ade2b7ca688072b7b655539be827f
Yuvraj Saxena <learningchessfor@gmail.com>
2021-09-27 23:00:39 +0530

    changed inital javascript

---
+++ script.js
@@ -0,0 +1,47 @@
+// using if statements
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//show input error
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//show success outline
+function showSuccess(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//email validation
+function validateEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+function checkRequired(arr) {
+    arr.forEach(function(input){
+        if(input.value.trim() === ''){
+            showError(input, input.id.charAt(0).toUpperCase()+input.id.slice(1)+' is required'); // concatenation
+        } else{
+            showSuccess(input);
+        }
+    })
+}
+
+// Event listers
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+
+    checkRequired([username, email, password, password2]);
+})
